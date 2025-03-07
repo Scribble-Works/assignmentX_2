9fd0ce117d54b5a83b58525234288ce7b6d49f20
aurictorkornoo <datauric@gmail.com>
2025-03-07 06:37:46 +0000

    Update questionaire.vue

--- components/questionaire.vue
+++ components/questionaire.vue
@@ -24,7 +24,7 @@ const submitFeedback = async () => {
 };
 </script>
 <template>
-    <div style="height: 40dvh; width: 100%; overflow-y: auto;" ref="scrl">
+    <div style="height: 40dvh; width:auto; overflow-y: auto;" ref="scrl">
         <h3 class="text-justify">
             <strong style="font-size: 2em;">Welcome to AssignmentX Beta!</strong><br>
             Thank you for participating in the AssignmentX beta test! AssignmentX is a web extension designed to
@@ -34,41 +34,36 @@ const submitFeedback = async () => {
         </h3>
         <br>
         <v-form @submit.prevent="submitFeedback">
-            <v-row>
-                <v-col>
-                    <v-label style="color: black; font-size: 1.3em;">
-                        <strong>Which of the following best describes your role?</strong>
-                    </v-label>
-                    <v-select v-model="role" label="Role" variant="solo-filled"
-                        :items="['Parent', 'Student', 'Educator', 'Other']"></v-select>
-                </v-col>
-                <v-col>
-                    <v-label style="color: black; font-size: 1.3em;">
-                        <strong>Overall, how would you rate your experience with AssignmentX?</strong>
-                    </v-label>
-                    <v-select v-model="experience" label="Experience" variant="solo-filled"
-                        :items="['Excellent', 'Good', 'Neutral', 'Fair', 'Poor']"></v-select>
-                </v-col>
-                <v-col>
-                    <v-label style="color: black; font-size: 1.3em;">
-                        <strong>Please share any specific experiences or observations you had while using AssignmentX.
-                            <br>
-                            What
-                            did you like? What could be improved?</strong>
-                    </v-label>
-                    <v-textarea v-model="oXperience" label="Observations and Recommedations" variant="solo-filled"></v-textarea>
-                </v-col>
-                <v-col>
-                    <v-label style="color: black; font-size: 1.3em;">
-                        <strong>
-                            Join our waitlist for Version 2.0!
-                            Be the first to access new updates and exclusive features. <br> Enter your email
-                            below!
-                        </strong>
-                    </v-label>
-                    <v-text-field v-model="email" variant="solo-filled" label="Email"></v-text-field>
-                </v-col>
-            </v-row>
+
+            <label style="color: black;">
+                <strong>Which of the following best describes your role?</strong>
+            </label>
+            <v-select v-model="role" label="Role" variant="solo-filled"
+                :items="['Parent', 'Student', 'Educator', 'Other']"></v-select>
+
+            <label style="color: black;">
+                <strong>Overall, how would you rate your experience with AssignmentX?</strong>
+            </label>
+            <v-select v-model="experience" label="Experience" variant="solo-filled"
+                :items="['Excellent', 'Good', 'Neutral', 'Fair', 'Poor']"></v-select>
+
+            <label style="color: black;">
+                <strong>Please share any specific experiences or observations you had while using AssignmentX.
+                    <br>
+                    What
+                    did you like? What could be improved?</strong>
+            </label>
+            <v-textarea v-model="oXperience" label="Observations and Recommedations" variant="solo-filled"></v-textarea>
+
+            <label style="color: black;">
+                <strong>
+                    Join our waitlist for Version 2.0!
+                    Be the first to access new updates and exclusive features. <br> Enter your email
+                    below!
+                </strong>
+            </label>
+            <v-text-field v-model="email" variant="solo-filled" label="Email"></v-text-field>
+
             <v-btn type="submit" color="primary">Submit</v-btn><br>
         </v-form>
     </div>
