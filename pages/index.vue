fe8f8be6f76501ef7b870a2b8c5dbeb65ca96dd1
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-05-17 05:26:33 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+const user = useSupabaseUser();
 </script>
 <template>
     <div>
@@ -9,8 +10,13 @@
             <div class="text-center text-white relative z-10 p-4 max-w-lg mx-auto">
                 <h1 class="text-4xl md:text-6xl font-bold">Unlock Learning with AssignmentX</h1>
                 <p class="mt-4 text-lg md:text-xl">Scan your workbook QR code to access an abundance of learning
-                    resources.</p>
-                <NuxtLink to="/auth" class="mt-6 inline-block bg-blue-500 text-white py-2 px-4 rounded">Get Started</NuxtLink>
+                    resources.</p><br>
+                <div v-if="!user">
+                    <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Get Started</NuxtLink>
+                </div>
+                <div v-else>
+                    <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Get Started</NuxtLink>
+                </div>
             </div>
         </section>
 
@@ -23,19 +29,37 @@
                         <i class="fas fa-video fa-3x text-blue-500 mb-4"></i>
                         <h5 class="text-xl font-bold mb-2">Video Lessons</h5>
                         <p class="mb-4">Access interactive video tutorials on various topics.</p>
-                        <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
+                        <div v-if="!user">
+                            <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
+                        </div>
+                        <div v-else>
+                            <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Explore
+                            </NuxtLink>
+                        </div>
                     </div>
                     <div class="card bg-white shadow-lg p-6 rounded">
                         <i class="fas fa-book fa-3x text-green-500 mb-4"></i>
                         <h5 class="text-xl font-bold mb-2">Concept Notes</h5>
                         <p class="mb-4">Detailed study notes to deepen your knowledge.</p>
-                        <NuxtLink to="/auth" class="bg-green-500 text-white py-2 px-4 rounded">Read</NuxtLink>
+                        <div v-if="!user">
+                            <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
+                        </div>
+                        <div v-else>
+                            <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Explore
+                            </NuxtLink>
+                        </div>
                     </div>
                     <div class="card bg-white shadow-lg p-6 rounded">
                         <i class="fas fa-question-circle fa-3x text-yellow-500 mb-4"></i>
                         <h5 class="text-xl font-bold mb-2">Quizzes</h5>
                         <p class="mb-4">Test yourself with interactive quizzes and challenges.</p>
-                        <NuxtLink to="/auth" class="bg-yellow-500 text-white py-2 px-4 rounded">Start</NuxtLink>
+                        <div v-if="!user">
+                            <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
+                        </div>
+                        <div v-else>
+                            <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Explore
+                            </NuxtLink>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,7 +71,8 @@
                 <h2 class="text-3xl font-bold mb-4">Don't Have the Book Yet?</h2>
                 <p class="text-lg mb-6">Get your Assignment Workbook now and unlock a world of learning with QR code
                     scanning!</p>
-                <NuxtLink href="https://wa.me/c/233201001041" class="bg-white text-yellow-500 py-2 px-4 rounded">Buy the Book</NuxtLink>
+                <NuxtLink href="https://wa.me/c/233201001041" class="bg-white text-yellow-500 py-2 px-4 rounded">Buy the
+                    Book</NuxtLink>
             </div>
         </section>
 
@@ -55,7 +80,12 @@
         <section class="cta bg-blue-500 text-white py-12 text-center">
             <div class="container mx-auto">
                 <h2 class="text-3xl font-bold mb-4">Ready to take your learning to the next level?</h2>
-                <NuxtLink to="/auth" class="bg-white text-blue-500 py-2 px-4 rounded">Get Started</NuxtLink>
+                <div v-if="!user">
+                    <NuxtLink to="/auth" class="bg-white text-blue-500 py-2 px-4 rounded">Get Started</NuxtLink>
+                </div>
+                <div v-else>
+                    <NuxtLink to="/workbook/" class="bg-white text-blue-500 py-2 px-4 rounded">Get Started</NuxtLink>
+                </div>
             </div>
         </section>
     </div>
