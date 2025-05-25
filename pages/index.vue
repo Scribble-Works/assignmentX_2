3b2952c6e6b1d4ae24fe1ca0768c1f278d6c90f0
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-05-25 21:40:38 +0000

    v2 in progress

--- pages/index.vue
+++ pages/index.vue
@@ -8,85 +8,68 @@ const user = useSupabaseUser();
             style="background-image: url('/img/img1.jpg');">
             <div class="absolute inset-0 bg-black opacity-50"></div>
             <div class="text-center text-white relative z-10 p-4 max-w-lg mx-auto">
-                <h1 class="text-4xl md:text-6xl font-bold">Unlock Learning with AssignmentX</h1>
+                <h1 class="text-4xl md:text-6xl font-bold">Unlock Learning with <div class="asign">AssignmentX</div></h1>
                 <p class="mt-4 text-lg md:text-xl">Scan your workbook QR code to access an abundance of learning
                     resources.</p><br>
                 <div v-if="!user">
-                    <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Get Started</NuxtLink>
+                    <v-btn to="/auth" size="large" color="blue">Get Started</v-btn>
                 </div>
                 <div v-else>
-                    <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Get Started</NuxtLink>
+                    <v-btn to="/workbook/" size="large" color="blue">Get Started</v-btn>
                 </div>
             </div>
         </section>
 
-        <!-- Feature Section -->
-        <section class="features py-12 bg-gray-100">
-            <div class="container mx-auto text-center">
-                <h2 class="text-3xl font-bold mb-8">Explore Our Features</h2>
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-                    <div class="card bg-white shadow-lg p-6 rounded">
-                        <i class="fas fa-video fa-3x text-blue-500 mb-4"></i>
-                        <h5 class="text-xl font-bold mb-2">Video Lessons</h5>
-                        <p class="mb-4">Access interactive video tutorials on various topics.</p>
-                        <div v-if="!user">
-                            <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
-                        </div>
-                        <div v-else>
-                            <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Explore
-                            </NuxtLink>
-                        </div>
-                    </div>
-                    <div class="card bg-white shadow-lg p-6 rounded">
-                        <i class="fas fa-book fa-3x text-green-500 mb-4"></i>
-                        <h5 class="text-xl font-bold mb-2">Concept Notes</h5>
-                        <p class="mb-4">Detailed study notes to deepen your knowledge.</p>
-                        <div v-if="!user">
-                            <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
-                        </div>
-                        <div v-else>
-                            <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Explore
-                            </NuxtLink>
-                        </div>
-                    </div>
-                    <div class="card bg-white shadow-lg p-6 rounded">
-                        <i class="fas fa-question-circle fa-3x text-yellow-500 mb-4"></i>
-                        <h5 class="text-xl font-bold mb-2">Quizzes</h5>
-                        <p class="mb-4">Test yourself with interactive quizzes and challenges.</p>
-                        <div v-if="!user">
-                            <NuxtLink to="/auth" class="bg-blue-500 text-white py-2 px-4 rounded">Explore</NuxtLink>
-                        </div>
-                        <div v-else>
-                            <NuxtLink to="/workbook/" class="bg-blue-500 text-white py-2 px-4 rounded">Explore
-                            </NuxtLink>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <p class="text-uppercase">what we do</p>
+                    <h2>Struggling to understand your textbook?</h2><br>
+                    <p>We've got you covered. <br> Clear explanations, helpful examples, and extra insights - all designed to help you master your subject.</p>
+                </v-col>
+                <v-col class="d-flex flex-column" cols="6">
+                    <v-row>
+                        <v-col cols="12">
+                            <v-sheet height="600" class="d-flex" rounded>
+                                <v-img src="/img/home1.jpg"></v-img>
+                            </v-sheet>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-sheet height="300" class="d-flex" rounded>
+                                <v-img src="/img/home2.jpg"></v-img>
+                            </v-sheet>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-sheet height="300" class="d-flex" rounded>
+                                <v-img src="/img/home3.jpg"></v-img>
+                            </v-sheet>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
 
-        <!-- Book Purchase Section -->
-        <section class="book-purchase py-12 bg-yellow-500 text-white text-center">
-            <div class="container mx-auto">
-                <h2 class="text-3xl font-bold mb-4">Don't Have the Book Yet?</h2>
-                <p class="text-lg mb-6">Get your Assignment Workbook now and unlock a world of learning with QR code
-                    scanning!</p>
-                <NuxtLink href="https://wa.me/c/233201001041" class="bg-white text-yellow-500 py-2 px-4 rounded">Buy the
-                    Book</NuxtLink>
-            </div>
-        </section>
 
-        <!-- Call to Action Section -->
-        <section class="cta bg-blue-500 text-white py-12 text-center">
-            <div class="container mx-auto">
-                <h2 class="text-3xl font-bold mb-4">Ready to take your learning to the next level?</h2>
-                <div v-if="!user">
-                    <NuxtLink to="/auth" class="bg-white text-blue-500 py-2 px-4 rounded">Get Started</NuxtLink>
-                </div>
-                <div v-else>
-                    <NuxtLink to="/workbook/" class="bg-white text-blue-500 py-2 px-4 rounded">Get Started</NuxtLink>
-                </div>
+            <div>
+                <p class="text-center text-uppercase">what we do</p>
+                <h1 class="text-center">Getting Started</h1><br>
+                <v-row>
+                    <v-col>
+                        <v-sheet>
+                            <v-img src="/img/home4.jpg"></v-img>
+                        </v-sheet>
+                    </v-col>
+                    <v-col>
+                        
+                    </v-col>
+                </v-row>
             </div>
-        </section>
+        </v-container>
+        
     </div>
 </template>
+<style>
+.asign {
+    color: rgb(255, 187, 0);
+    
+}
+</style>
