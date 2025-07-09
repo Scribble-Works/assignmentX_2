97f00e6f4096dc1b932442fb2036f31659178831
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-07-09 14:05:26 +0000

    adding facilitator resources

--- components/cardResources.vue
+++ components/cardResources.vue
@@ -1,10 +1,26 @@
 <script setup>
-
+const props = defineProps({
+    title: String,
+    description: String,
+    link: String
+});
 </script>
 <template>
     <div>
-        <v-card>
-            
-        </v-card>
+        <NuxtLink :to="props.link">
+            <v-card>
+                <v-container>
+                    <v-row>
+                        <v-col cols="2">
+                            <v-icon style="font-size: 3em; color: #2096F3;">mdi-book-open-blank-variant-outline</v-icon>
+                        </v-col>
+                        <v-col>
+                            <h4 class="text-h5">{{props.title}}</h4>
+                            <p>{{ props.description }}</p>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card>
+        </NuxtLink>
     </div>
 </template>
---
+++ layouts/resources.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <theHeader />
+        <v-container>
+            <v-text-field prepend-inner-icon="mdi-magnify" label="Search chapters and topics..."
+                variant="outlined"></v-text-field><br>
+            <slot />
+        </v-container>
+        <theFooter />
+    </div>
+</template>
