2c9edc340e142acf80982d0075b8a92564abd2b3
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-07-23 05:47:52 +0000

    forgot password

--- pages/forget.vue
+++ pages/forget.vue
@@ -8,10 +8,10 @@ const resetPassword = async () => {
     try {
         const { data, error } = await auth.resetPasswordForEmail(email.value,
             {
-                redirectTo: 'https://assignmentx.swph.tech/newpassword'
+                redirectTo: 'https://assignmentx.scribbleworks.tech/newpassword'
             }
         );
-
+        console.log(data);
         if (error) {
             alert('An error occurred. Please try again later.');
             console.error(error);
@@ -25,18 +25,34 @@ const resetPassword = async () => {
         console.error(error);
     }
 };
+
+const backLogin = () => {
+    router.push('/auth');
+};
 </script>
 <template>
-    <div class="d-flex flex-column fill-height justify-center align-center min-h-screen">
-        <v-card style="width: 80dvw;">
-            <form @submit.prevent="resetPassword">
-                <v-container>
-                    <v-label>Email</v-label>
-                    <v-text-field variant="outlined" v-model="email" type="email" placeholder="Enter your email" required></v-text-field>
-                    <v-btn type="submit">Reset</v-btn>
-                </v-container>
-            </form>
-        </v-card>
+    <div class="body">
+        <v-row>
+            <v-col class="mt-16 pt-16">
+                <form @submit.prevent="resetPassword">
+                    <v-container>
+                        <h3 class="text-h3" style="font-family: 'Inter', sans-serif;">Forgot password?</h3><br>
+                        <p style="font-family: 'Inter', sans-serif;">No worries, we'll send you reset instructions</p>
+                        <div class="mt-10">
+                            <v-label>Email</v-label>
+                            <v-text-field variant="outlined" v-model="email" type="email" placeholder="Enter your email"
+                                required></v-text-field>
+                            <v-btn color="grey-darken-3" style="width: 100%;" type="submit">Reset</v-btn><br>
+                            <v-btn @click="backLogin" class="mt-5" style="width: 100%;" variant="plain"><v-icon style="font-size: 2.5em; color: black;">mdi-keyboard-backspace</v-icon> Back to Login</v-btn>
+                        </div>
+                    </v-container>
+                </form>
+            </v-col>
+            <v-col>
+                <img src="/img/forget.png" alt="Forget Password">
+            </v-col>
+        </v-row>
+
 
         <v-dialog v-model="alert" width="auto">
             <v-card max-width="400" title="Reset Password" text="Please check your email for the reset link.">
@@ -47,3 +63,8 @@ const resetPassword = async () => {
         </v-dialog>
     </div>
 </template>
+<style>
+.body {
+    background: white;
+}
+</style>
