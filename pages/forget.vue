9accea090bdf40766102d69de784779744bc1fd6
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-07-24 13:43:50 +0000

    Frontend enhancement on forget password

--- pages/forget.vue
+++ pages/forget.vue
@@ -1,4 +1,7 @@
 <script setup>
+definePageMeta({
+    layout: 'auth',
+});
 const { auth } = useSupabaseClient();
 const user = useSupabaseUser();
 const router = useRouter();
@@ -6,11 +9,7 @@ const email = ref('');
 const alert = ref(false);
 const resetPassword = async () => {
     try {
-        const { data, error } = await auth.resetPasswordForEmail(email.value,
-            {
-                redirectTo: 'https://assignmentx.scribbleworks.tech/newpassword'
-            }
-        );
+        const { data, error } = await auth.resetPasswordForEmail(email.value);
         console.log(data);
         if (error) {
             alert('An error occurred. Please try again later.');
@@ -32,35 +31,41 @@ const backLogin = () => {
 </script>
 <template>
     <div class="body">
-        <v-row>
-            <v-col class="mt-16 pt-16">
-                <form @submit.prevent="resetPassword">
-                    <v-container>
-                        <h3 class="text-h3" style="font-family: 'Inter', sans-serif;">Forgot password?</h3><br>
-                        <p style="font-family: 'Inter', sans-serif;">No worries, we'll send you reset instructions</p>
-                        <div class="mt-10">
-                            <v-label>Email</v-label>
-                            <v-text-field variant="outlined" v-model="email" type="email" placeholder="Enter your email"
-                                required></v-text-field>
-                            <v-btn color="grey-darken-3" style="width: 100%;" type="submit">Reset</v-btn><br>
-                            <v-btn @click="backLogin" class="mt-5" style="width: 100%;" variant="plain"><v-icon style="font-size: 2.5em; color: black;">mdi-keyboard-backspace</v-icon> Back to Login</v-btn>
-                        </div>
-                    </v-container>
-                </form>
-            </v-col>
-            <v-col>
-                <img src="/img/forget.png" alt="Forget Password">
-            </v-col>
-        </v-row>
+        <v-container>
+            <v-row>
+                <v-col cols="" lg="6" md="12" sm="12" class="mt-16 pt-16">
+                    <form @submit.prevent="resetPassword">
+                        <v-container>
+                            <h3 class="text-h3" style="font-family: 'Inter', sans-serif;">Forgot password?</h3><br>
+                            <p style="font-family: 'Inter', sans-serif;">No worries, we'll send you reset instructions
+                            </p>
+                            <div class="mt-10">
+                                <v-label>Email</v-label>
+                                <v-text-field variant="outlined" v-model="email" type="email"
+                                    placeholder="Enter your email" required></v-text-field>
+                                <v-btn color="grey-darken-3" style="width: 100%;" type="submit">Reset
+                                    Password</v-btn><br>
+                                <v-btn @click="backLogin" class="mt-5" style="width: 100%;" variant="plain"><v-icon
+                                        style="font-size: 2.5em; color: black;">mdi-keyboard-backspace</v-icon> Back to
+                                    Login</v-btn>
+                            </div>
+                        </v-container>
+                    </form>
+                </v-col>
+                <v-col cols="" lg="6" md="12" sm="12">
+                    <img src="/img/forget.png" alt="Forget Password">
+                </v-col>
+            </v-row>
 
 
-        <v-dialog v-model="alert" width="auto">
-            <v-card max-width="400" title="Reset Password" text="Please check your email for the reset link.">
-                <template v-slot:actions>
-                    <v-btn class="ms-auto" text="Ok" @click="alert = false"></v-btn>
-                </template>
-            </v-card>
-        </v-dialog>
+            <v-dialog v-model="alert" width="auto">
+                <v-card max-width="400" title="Reset Password" text="Please check your email for the reset link.">
+                    <template v-slot:actions>
+                        <v-btn class="ms-auto" text="Ok" @click="alert = false"></v-btn>
+                    </template>
+                </v-card>
+            </v-dialog>
+        </v-container>
     </div>
 </template>
 <style>
--- pages/newpassword.vue
+++ pages/newpassword.vue
@@ -1,48 +1,82 @@
 <script setup>
+definePageMeta({
+    layout: 'auth',
+});
 const { auth } = useSupabaseClient();
 const user = useSupabaseUser();
 const router = useRouter();
 
 const newPassword = ref('');
-const confirmNewPassword = ref('');
-const resetPassword = async () => {
-    try {
-        if (newPassword.value !== confirmNewPassword.value) {
-            alert('Passwords do not match');
-            return;
-        }
-        const { data, error } = await auth.updateUser({
-            password: newPassword.value
-        });
 
-        if (error) {
-            alert('An error occurred. Please try again later.');
-            console.error(error);
-        } else {
-            alert('Password updated successfully!');
-            router.push('/auth');
-        }
-    } catch (error) {
-        alert('An error occurred. Please try again later.');
-        console.error(error);
-    }
+const confirmNewPassword = ref('');
+
+const backLogin = () => {
+    router.push('/auth');
 };
+watchEffect(() => {
+    auth.onAuthStateChange(async (event, session) => {
+        if (event == "PASSWORD_RECOVERY") {
+            const { data, error } = await auth.updateUser({ password: newPassword.value })
+        }
+    })
+})
+// const resetPassword = async () => {
+//     try {
+//         if (newPassword.value !== confirmNewPassword.value) {
+//             alert('Passwords do not match');
+//             return;
+//         }
+//         const { data, error } = await auth.updateUser({
+//             password: newPassword.value
+//         });
+
+//         if (error) {
+//             alert('An error occurred. Please try again later.');
+//             console.error(error);
+//         } else {
+//             alert('Password updated successfully!');
+//             router.push('/auth');
+//         }
+//     } catch (error) {
+//         alert('An error occurred. Please try again later.');
+//         console.error(error);
+//     }
+// };
 </script>
 <template>
-    <div class="d-flex flex-column fill-height justify-center align-center min-h-screen">
-        <v-card elevation="1">
-            <v-form @submit.prevent="resetPassword">
-                <v-container style="width: 80dvw;">
-                    <v-label>New Password</v-label>
-                    <v-text-field variant="outlined" type="password" v-model="newPassword"></v-text-field>
-                    <br>
-                    <v-label>Confirm New Password</v-label>
-                    <v-text-field variant="outlined" type="password" v-model="confirmNewPassword"></v-text-field>
-                    <br>
-                    <v-btn type="submit">Create New Password</v-btn>
-                </v-container>
-            </v-form>
-        </v-card>
-
+    <div class="body">
+        <v-container>
+            <v-row>
+                <v-col class="mt-16 pt-10">
+                    <v-form @submit.prevent="">
+                        <v-container>
+                            <h3 style="font-family: 'Inter', sans-serif; font-weight: bold;"
+                                class="text-h3 text-center">Set New Password</h3><br>
+                            <p class="text-center">Must Be At Least 8 Characters</p>
+                            <v-label>New Password</v-label>
+                            <v-text-field variant="outlined" type="password" v-model="newPassword"></v-text-field>
+                            <br>
+                            <v-label>Confirm New Password</v-label>
+                            <v-text-field variant="outlined" type="password"
+                                v-model="confirmNewPassword"></v-text-field>
+                            <br>
+                            <v-btn style="width: 100%;" color="grey-darken-3" type="submit">Create New
+                                Password</v-btn><br>
+                            <v-btn @click="backLogin" class="mt-5" style="width: 100%;" variant="plain"><v-icon
+                                    style="font-size: 2.5em; color: black;">mdi-keyboard-backspace</v-icon> Back to
+                                Login</v-btn>
+                        </v-container>
+                    </v-form>
+                </v-col>
+                <v-col>
+                    <v-img src="/img/newpass.png" height="800"></v-img>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
+<style>
+.body {
+    background-color: white;
+}
+</style>
