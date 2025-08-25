a60261d22696413c84ff8cc29efb7e4cd0eabb29
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-08-25 22:44:00 +0000

    fixed bug for onboarding

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,53 +2,23 @@
 const user = useSupabaseUser();
 const client = useSupabaseClient();
 
-// const dialog = ref(false);
 
-// const profile = ref(null);
+let onboarding = { data: null };
 
-// onMounted(async () => {
-//     try {
-//         if (user.value && profile.value === null) {
-//             const { data } = await client.from("profiles").select('*').eq("id", user.value.id).single();
-//             if (data) {
-//                 profile.value = data;
-//             }
-//         }
-//     } catch (error) {
-//         console.error('Error fetching profile data:', error);
-//     }
-// });
-
-
-// watch(
-//     () => [user.value, profile.value],
-//     ([userVal, profileVal]) => {
-//         try {
-//             if (userVal && profileVal === null) {
-//                 dialog.value = true;
-//             } else {
-//                 dialog.value = false;
-//             }
-//         } catch (error) {
-//             console.error('Error in watch function:', error);
-//         }
-//     },
-//     { immediate: true }
-// );
-
-// const openBioData = () => {
-//     navigateTo('/bio');
-// };
-const onboarding = await client.from("onboarding")
-  .select("*")
-  .eq("id", user.value.id)
-  .single();
-
-onMounted(() => {
-  if (user.value && onboarding.data == null) {
-    navigateTo('/onboarding');
-  }
+onMounted(async () => {
+    if (user.value && user.value.id) {
+        const { data } = await client.from("onboarding")
+            .select("*")
+            .eq("id", user.value.id)
+            .single();
+        onboarding.data = data;
+        if (onboarding.data == null) {
+            navigateTo('/onboarding');
+        }
+    }
 });
+
+
 </script>
 <template>
     <div class="min-h-screen flex flex-col">
@@ -57,16 +27,5 @@ onMounted(() => {
             <slot />
         </main>
         <theFooter />
-
-        <!-- <div v-if="dialog" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div class="bg-white rounded-lg shadow-lg max-w-md w-full p-6">
-            <h2 class="text-xl font-semibold mb-4">Fill Bio Data</h2>
-            <p class="mb-6">Please Fill Bio Data to use the app.</p>
-            <div class="flex justify-end space-x-2">
-                <button @click="openBioData"
-                class="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition">Bio Data</button>
-            </div>
-            </div>
-        </div> -->
     </div>
 </template>
