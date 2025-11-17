3364c05ca438d884d4220b76a419ce366e6eaca8
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-11-17 19:27:35 +0000

    Add iframe permissions to assignment preview pages
    Added 'allow-scripts' and 'allow-same-origin' attributes to the iframe elements in first, second, and last assignment preview pages to enable enhanced functionality and compatibility for embedded PDF previews.

--- pages/first-assignment-preview.vue
+++ pages/first-assignment-preview.vue
@@ -13,33 +13,38 @@
     <div class="max-w-7xl mx-auto px-4 py-8">
       <!-- Header -->
       <div className="text-left mb-16">
-        <h1
-          className="text-4xl md:text-5xl font-bold text-slate-800 mb-6"
-        >
+        <h1 className="text-4xl md:text-5xl font-bold text-slate-800 mb-6">
           The First Assignment Preview
         </h1>
         <h2 className="text-xl md:text-2xl font-semibold text-slate-700 mb-6">
           Take a Peek Inside the Workbook
         </h2>
         <p className="text-sm text-slate-600 max-w-4xl ">
-          Explore a few sample pages from our math workbook. Get a feel for how the
-          Assignment Maths Workbooks brings math to life through real-world examples,
-          storytelling, and guided practice.
+          Explore a few sample pages from our math workbook. Get a feel for how
+          the Assignment Maths Workbooks brings math to life through real-world
+          examples, storytelling, and guided practice.
         </p>
       </div>
 
       <!-- Carousel -->
       <!-- <v-carousel hide-delimiters height="950"> -->
-        <!-- <v-carousel-item v-for="(img, index) in previewImages" :key="index"> -->
-          <div class="h-full w-full flex justify-center items-center">
-            <iframe height="650" width="900" src="https://yvzftzhtqcddqadufjue.supabase.co/storage/v1/object/public/book-preview/First%20Assignment%202025%20first%2030%20pages.pdf" frameborder="0"></iframe>
-            <!-- <img
+      <!-- <v-carousel-item v-for="(img, index) in previewImages" :key="index"> -->
+      <div class="h-full w-full flex justify-center items-center">
+        <iframe
+          height="650"
+          width="900"
+          src="https://yvzftzhtqcddqadufjue.supabase.co/storage/v1/object/public/book-preview/First%20Assignment%202025%20first%2030%20pages.pdf"
+          allow-scripts
+          allow-same-origin
+          frameborder="0"
+        ></iframe>
+        <!-- <img
               :src="img"
               alt="Preview"
               class="h-[800px] w-auto rounded-l shadow-lg object-contain"
             /> -->
-          </div>
-        <!-- </v-carousel-item> -->
+      </div>
+      <!-- </v-carousel-item> -->
       <!-- </v-carousel> -->
     </div>
   </div>
