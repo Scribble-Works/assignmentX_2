7916db7eb0831c0ad07608c59357d88cbcf5e6d8
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2025-12-10 16:37:21 +0000

    Switch assignment previews to dynamic image carousel
    Replaced static iframe previews with dynamic carousels that fetch image data from Supabase for both last and second assignment preview pages. This improves flexibility and user experience by displaying assignment preview images directly from the database.

--- pages/last-assignment-preview.vue
+++ pages/last-assignment-preview.vue
@@ -1,11 +1,12 @@
 <script setup>
-// const previewImages = [
-//   "/img/1_Identifying even, odd, prime and composite numbers_1.png",
-//   "/img/2_Finding prime factors of natural numbers_2.png",
-//   "/img/3_Finding the HCF of natural numbers_3.png",
-//   "/img/4_Finding the LCM of natural numbers_4.png",
-//   "/img/5_Addition and subtraction of whole numbers_5.png",
-// ];
+const client = useSupabaseClient();
+const { data: previewImages } = await client
+  .from("book-preview")
+  .select()
+  .eq("id", 3);
+
+const imagesArray = previewImages[0].files.img;
+console.log(imagesArray);
 </script>
 
 <template>
@@ -27,25 +28,15 @@
       </div>
 
       <!-- Carousel -->
-      <!-- <v-carousel hide-delimiters height="950"> -->
-      <!-- <v-carousel-item v-for="(img, index) in previewImages" :key="index"> -->
-      <div class="h-full w-full flex justify-center items-center">
-        <iframe
-          height="650"
-          width="900"
-          src="https://yvzftzhtqcddqadufjue.supabase.co/storage/v1/object/public/book-preview/last%20Assignment%202025%20first%2030%20pages.pdf"
-          allow-scripts
-          allow-same-origin
-          frameborder="0"
-        ></iframe>
-        <!-- <img
-              :src="img"
-              alt="Preview"
-              class="h-[800px] w-auto rounded-l shadow-lg object-contain"
-            /> -->
-      </div>
-      <!-- </v-carousel-item> -->
-      <!-- </v-carousel> -->
+      <v-carousel hide-delimiters height="950">
+        <v-carousel-item v-for="img in imagesArray" :key="img.id">
+            <img
+            :src="img.link"
+            alt="Preview"
+            class="w-full h-full rounded-l shadow-lg object-contain"
+            />
+        </v-carousel-item>
+      </v-carousel>
     </div>
   </div>
 </template>
