14e0eb2dc9f76ff1d465eb05a09aa36c5f6c631d
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2026-05-02 16:30:25 +0000

    Standardize DB queries and order substrands
    Refactor Supabase queries across multiple workbook strand pages to a consistent multiline style and double-quote usage. Add explicit ordering for substrand lists (.order("id", { ascending: true })) where applicable and use a computed sort for preassignment_workbook1 to ensure stable ordering. Also tidy templates (consistent indentation, lowercase hex color, self-closing <br />) and minor console.log formatting fixes across the six strand pages.

--- pages/learning-modules/preassignment_workbook3/strand-[id]/index.vue
+++ pages/learning-modules/preassignment_workbook3/strand-[id]/index.vue
@@ -6,48 +6,76 @@ const route = useRoute();
 const client = useSupabaseClient();
 const id = route.params.id;
 
-const { data: workbook } = await client.from('pre-assignment_Workbook3').select().eq('id', id);
-const { data: strand1 } = await client.from('preassignment_workbook3_substrands_contents').select().eq('id', id);
+const { data: workbook } = await client
+  .from("pre-assignment_Workbook3")
+  .select()
+  .eq("id", id);
+const { data: strand1 } = await client
+  .from("preassignment_workbook3_substrands_contents")
+  .select()
+  .eq("id", id);
 const substrand_ref = strand1[0].substrand_ref;
-const { data: substrands } = await client.from('preassignment_workbook3_strand_substrands_lists').select().eq('strand_ref', id)
+const { data: substrands } = await client
+  .from("preassignment_workbook3_strand_substrands_lists")
+  .select()
+  .eq("strand_ref", id)
+  .order("id", { ascending: true });
 const conceptNote = workbook[0].concept_notes;
 const strandNumber = substrands[0].strand_ref;
-const { data: strandtitle } = await client.from('pre-assignment_Workbook3').select().eq('id', strandNumber);
+const { data: strandtitle } = await client
+  .from("pre-assignment_Workbook3")
+  .select()
+  .eq("id", strandNumber);
 const title = strandtitle[0].strand_name;
 const vid = strandtitle[0].vid;
 console.log(strand1);
 console.log(substrands);
-console.log(workbook)
+console.log(workbook);
 console.log(strandtitle);
-
 </script>
 <template>
-    <div class="mt-5" style="height: auto;">
-        <v-container>
-            <h1 class="text-left font-weight-bold text-uppercase text-bold" style="font-size: 1.5em; color: #3E4F5C;">
-                STRAND
-                {{ strandNumber }} - {{ title }}
-            </h1>
-            <introvid :intro="vid" />
-            <div class="mt-10">
-                <v-row class="mt-n5" v-for="substrand in substrands" :key="substrand.id">
-                    <v-col>
-                        <NuxtLink
-                            :to="'/learning-modules/preassignment_workbook3/strand-' + substrand.strand_ref + '/substrand-' + substrand.route + '/'">
-                            <v-card>
-                                <v-card-title class="font-weight-light text-left">{{ substrand.title }}</v-card-title>
-                            </v-card>
-                        </NuxtLink>
-                        <v-spacer></v-spacer>
-                        <br>
-                    </v-col>
-                </v-row>
-            </div>
-        </v-container>
-    </div>
+  <div class="mt-5" style="height: auto">
+    <v-container>
+      <h1
+        class="text-left font-weight-bold text-uppercase text-bold"
+        style="font-size: 1.5em; color: #3e4f5c"
+      >
+        STRAND
+        {{ strandNumber }} - {{ title }}
+      </h1>
+      <introvid :intro="vid" />
+      <div class="mt-10">
+        <v-row
+          class="mt-n5"
+          v-for="substrand in substrands"
+          :key="substrand.id"
+        >
+          <v-col>
+            <NuxtLink
+              :to="
+                '/learning-modules/preassignment_workbook3/strand-' +
+                substrand.strand_ref +
+                '/substrand-' +
+                substrand.route +
+                '/'
+              "
+            >
+              <v-card>
+                <v-card-title class="font-weight-light text-left">{{
+                  substrand.title
+                }}</v-card-title>
+              </v-card>
+            </NuxtLink>
+            <v-spacer></v-spacer>
+            <br />
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </div>
 </template>
 <style>
 body {
-    background-color: #F6F6F6;
+  background-color: #f6f6f6;
 }
 </style>
